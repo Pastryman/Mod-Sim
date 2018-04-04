e83f316bf3704aa59ceb972b958a9a6a0e92b7e6
florisjonkman <florisjonkman@gmail.com>
2018-04-04 10:46:31 +0200

    Lezen/Printen verwerkt

--- Week 7/sudoku.cpp
+++ Week 7/sudoku.cpp
@@ -4,40 +4,57 @@
 #include <math.h>
 #include <iostream>
 #include "mt19937.h"
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
 
 /* Initializing sudoku*/
 const int size      = 9;
 int sudoku[size][size];
-const char*  sodoku_filename = "sudoku.csv";
+const char*  init_filename = "sudoku.dat";
 
 /* Parameters */
 const double T = 0.5;
 
 
 void read_sudoku(void){
-    FILE* fp = fopen(sodoku_filename, "r");
+    FILE* fp = fopen(init_filename, "r");
     int r,c;
     for(r = 0; r < size; ++r){
         for(c = 0; c < size; ++c){
-            fscanf(fp, "%d;", &sudoku[r][c]);
             if(c==size-1){
                 fscanf(fp, "%d\n", &sudoku[r][c]);
+            }
+            else{
+                fscanf(fp, "%d\t", &sudoku[r][c]);
             }
         }
     }
     fclose(fp);
 }
 
-int main() {
-    read_sudoku();
-
+void print_sudoku(void){
     for(int r = 0; r < size; ++r){
+        if(r*r % size == 0){std::cout << "---------------------\n"; }
         for(int c = 0; c < size; ++c){
-            if(c % 3 == 0){std::cout << "|";}
-            std::cout << sudoku[r][c] << "\t";
+            if(c*c % size == 0){std::cout << "|";}
             if(c==size-1){
-                std::cout << sudoku[r][c] << "\n";;
+                std::cout << sudoku[r][c] << "|\n";;
+            }
+            else{
+                std::cout << sudoku[r][c] << " ";
             }
         }
     }
+}
+
+
+int main() {
+
+    read_sudoku();
+
+    print_sudoku();
+
+    return 1;
 }
