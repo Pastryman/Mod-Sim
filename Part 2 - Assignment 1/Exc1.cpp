079dcee20e635db877b7c3e26760188c681df10e
TomNiessen <niessen.tom@gmail.com>
2018-04-12 16:41:30 +0200

    werken aan assignment 1, debuggen nodig!

--- Part 2 - Assignment 1/Exc1.cpp
+++ Part 2 - Assignment 1/Exc1.cpp
@@ -1,64 +1,308 @@
-/*  Mod&Sim 2018 week 2, Exercise 1
- *
- *  Authors: Tom Niessen & Floris Jonkman
- * */
-
+#include <stdio.h>
+#include <time.h>
+#include <assert.h>
+#include <math.h>
+#include "mt19937.h"
 #include <iostream>
 #include <fstream>
-using namespace std;
+#include <algorithm>
 
-// Initialize a stream to our file
-ofstream myfile;
 
-/*Simulation variables*/
-const float latticeSize = 1;  // Lattice spacing (size of the unit cell)
-const float particleSize = 1; // Size of a particle
-const int Nx = 10; // Amount of particles in the x direction
-const int Ny = 10; // Amount of particles in the y direction
-const int Nz = 10; // Amount of particles in the z direction
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
-// Method: Cubic
-// Input: float dx,dy,dz: displacement of the lattice cell with respect to the origin
-// Description: This method generates the coordinates of the particle from the coordinates of the cell
-//              This is cubic, so we only have 1 particle per cell
-//              The coords are then written to a file
-void Cubic(float dx, float dy, float dz)
-{
-    // Calculation of the coordinates of the particle in the cell
-    float x = dx*latticeSize;
-    float y = dy*latticeSize;
-    float z = dz*latticeSize;
-    // Writing the coordinates to our file (space separated)
-    myfile << x << " " << y << " " << z << " " << particleSize << "\n";
+#define NDIM 3
+#define N 1000
+
+
+//GIT TEST
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//// SET TO TRUE IF YOU WANT TO PRINT DETAILS (for debugging)
+const bool debug = 0;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/* Initialization variables */
+const int mc_steps = 200000;
+int output_steps = 100;
+const double packing_fraction = 0.3; //0.7
+const double diameter = 1.0;
+const double dt = 0.01;
+const int maxMeasure = 500;
+const double sim_time = 0.03;
+
+/* Volume change -deltaV, delta V */
+double delta  = 0.05;
+double deltaV = 0.05;
+
+/* Reduced pressure \beta P */
+const double temp = 1;
+const double betaP = 5;
+const char* init_filename = "fcc.dat";
+const double rcut = 2.5;
+double ecut;
+bool liquid = 1;
+
+
+
+/* Simulation variables */
+int n_particles = 0;
+double radius;
+double particle_volume;
+float r[N][NDIM];
+float v[N][NDIM];
+float F[N][NDIM];
+float F_old[N][NDIM];
+double box[NDIM];
+double PotE;
+
+/*
+ * Method: read_data
+ * Description:
+ *  Reads data from a file and stores the results in specified global variables
+ *  Processing of the file goes like this:
+ *  Line 1:      number of particles in the file
+ *  Line 2-4:    dimensions of the box
+ *  Line 5-end:  x, y, z, R     coordinates and size of particle (R is never used)
+ */
+void read_data(void){
+    // Open the file and print if the file cannot be opened
+    FILE* dataFile;
+    if((dataFile = fopen(init_filename,"r"))==NULL)
+        printf("file could not be opened\n");
+
+    // Read line 1: Number of particles
+    fscanf(dataFile, "%d", &n_particles);
+    if (debug){std::cout << "nParticles = " << n_particles << "\n";}
+
+    // Read lines 2-4: dimensions of the box
+    float boxDim1, boxDim2;
+    int i = 0;
+    while (i < NDIM)
+    {
+        fscanf(dataFile, "\n%f %f",&boxDim1, &boxDim2);
+        box[i]=abs(boxDim2 - boxDim1);
+        if (debug){std::cout << "boxdim" << i << " =" << box[i] << "\n";}
+        i++;
+    }
+
+    // Line 5-end: reading coordinates of the particles
+    // A "dummy_r" is created to store the value of the third component of each line
+    float dummy_r;
+    i=0;
+    // The number of lines to be read has to be smaller that n_particles or N
+    while (i < n_particles && i < N)
+    {
+        // Read the values on the line, the last value is ignored
+        fscanf(dataFile, "%f %f %f %*f",&r[i][0], &r[i][1],&dummy_r);
+        // Print the coordinates of the read particle (for debugging)
+        if (debug == 1){std::cout << "\n" << i+1 << ": x=" << r[i][0] << ", y=" << r[i][1];}
+        if (NDIM == 3){
+            r[i][2]=dummy_r;
+            if (debug){std::cout << ", z=" << r[i][2];}
+        }
+        i++;
+    }
+
+
 }
 
-int main() {
-    // Open our file
-    myfile.open("xyz.dat");
-    // Line 1 of the file: The total amount of particles
-    myfile << Nx*Ny*Nz << "\n";
-    // Lines 2-4: The dimensions of the box
-    myfile << 0 << " " << Nx*latticeSize << "\n";
-    myfile << 0 << " " << Ny*latticeSize << "\n";
-    myfile << 0 << " " << Nz*latticeSize << "\n";
+void write_data(double time){
+    char buffer[128];
+    sprintf(buffer, "coords_time%f.dat", time);
+    FILE* fp = fopen(buffer, "w");
+    int d, n;
+    fprintf(fp, "%d\n", n_particles);
+    for(d = 0; d < NDIM; ++d){
+        fprintf(fp, "%lf %lf\n",0.0,box[d]);
+    }
+    for(n = 0; n < n_particles; ++n){
+        for(d = 0; d < NDIM; ++d) fprintf(fp, "%lf\t", r[n][d]);
+        fprintf(fp, "%lf\n", diameter);
+    }
+    fclose(fp);
+}
 
-    // Loop over every cell in our box
-    // The x direction
-    for (int i = 0; i<Nx; i++)
-    {
-        //The y direction
-        for (int j = 0; j<Ny; j++)
-        {
-            // The z direction
-            for (int k = 0; k<Nz; k++)
-            {
-                // Generate the particle and write to file
-                Cubic(i,j,k);
+void set_packing_fraction(void){
+    double volume = 1.0;
+    int d, n;
+    for(d = 0; d < NDIM; ++d) volume *= box[d];
+    std::cout<<"\nold system volume = " <<volume;
+
+    double target_volume = (n_particles * particle_volume) / packing_fraction;
+    double scale_factor = pow(target_volume / volume, 1.0 / NDIM);
+
+    for(n = 0; n < n_particles; ++n){
+        for(d = 0; d < NDIM; ++d) r[n][d] *= scale_factor;
+    }
+    for(d = 0; d < NDIM; ++d) box[d] *= scale_factor;
+
+}
+
+void update_forces(){
+    for(int i = 0; i < n_particles; i++){
+        for (int j = 0; j < i; j++){
+            for (int d = 0; d<NDIM; d++){
+                // Distance between particles
+                double dist = r[i][d]-r[j][d];
+                // Periodic boundary conditions
+                dist = dist - (int)(2.0 * dist / box[d]) * box[d];
+                double dist2 = dist*dist;
+
+                if (dist2 < rcut*rcut){
+                    // Calculate the force on the particles using the LJ potential
+                    double r2i=1/dist2;
+                    double r6i=pow(r2i,3.);
+                    double ff = 48 * r2i * r6i*(r6i-0.5);
+                    // Apply the Force to the particles (in opposite direction)
+                    F[i][d] += ff*dist;
+                    F[j][d] -= ff*dist;
+                    // Update the potential energy
+                    PotE += 4*r6i*(r6i-1)-ecut;
+                }
             }
+
         }
     }
+}
 
-    // Close the file
-    myfile.close();
-    return 0;
+void update_kinematics(){
+    for(int n = 0; n < n_particles; ++n){
+        for(int d = 0; d < NDIM; ++d){
+            r[n][d] = float(r[n][d] + v[n][d]*dt + F[n][d]*dt*dt/(2.));
+            F_old[n][d] = F[n][d];
+            update_forces();
+            v[n][d]=v[n][d] + (F[n][d]+F_old[n][d])*dt/2.;
+        }
     }
+}
+
+void initialize_config() {
+    float sumv = 0;
+    float sumv2 = 0;
+
+    for (int d = 0; d < NDIM; d++){
+        sumv = 0;
+        for (int n = 0; n < n_particles; n++)
+        {
+            float v_temp = dsfmt_genrand()*2.-1.;
+            v[n][d] = v_temp;
+            sumv += v_temp;
+            sumv2 += v_temp*v_temp;
+        }
+        sumv2/=n_particles;
+        float fs = sqrt(3.*temp/sumv2);
+        //Total momentum (in each dimension) must be zero
+        for (int n = 0; n < n_particles; n++){
+            v[n][d]=(v[n][d]-sumv/double(n_particles))*fs;
+        }
+
+    }
+
+    //Total energy must be (3/2)*N*kb*T
+
+    for (int n = 0; n < n_particles; n++){
+
+    }
+
+}
+
+int main(int argc, char* argv[]){
+    std::cout << "\nBetaP = " << betaP << "\n";
+
+    radius = 0.5 * diameter;
+
+    if(NDIM == 3) particle_volume = M_PI * pow(diameter, 3.0) / 6.0;
+    else if(NDIM == 2) particle_volume = M_PI * pow(radius, 2.0);
+    else{
+        printf("Number of dimensions NDIM = %d, not supported.", NDIM);
+        return 0;
+    }
+
+    read_data();
+
+    if(n_particles == 0){
+        printf("Error: Number of particles, n_particles = 0.\n");
+        return 0;
+    }
+
+    ecut = 4.*(pow(rcut,-12.)-pow(rcut,-6.));
+
+    set_packing_fraction();
+
+    dsfmt_seed(time(NULL));
+
+    write_data(0);
+    initialize_config();
+    update_forces();
+
+    double time=0;
+    while(time<sim_time){
+        update_kinematics();
+        time+=dt;
+        std::cout << "time = " << time;
+        write_data(time);
+    }
+
+    /*char buffer[128];
+    if (liquid) {sprintf(buffer, "Exercise2_P%.0f_liquid.dat",betaP);}
+    else {sprintf(buffer, "Exercise2_P_fcc%.0f.dat",betaP);}
+    FILE* fp = fopen(buffer, "w");
+
+    printf("\n#Step \t Volume \t Move-acceptance\t Volume-acceptance");
+    fprintf(fp,"#Step \t Volume \t Move-acceptance\t Volume-acceptance");
+
+    int move_accepted = 0;
+    int vol_accepted = 0;
+    int step, n;
+    double volume_old = liquid?100000:0;
+    bool measure = 0;
+    int measurements = 0;
+    for(step = 0; step < mc_steps; ++step){
+        for(n = 0; n < n_particles; ++n){
+            move_accepted += move_particle();
+        }
+        vol_accepted += change_volume();
+
+        double moveRatio = double(move_accepted) / (double(n_particles) * double(output_steps));
+        double volumeRatio = double(vol_accepted) /  double(output_steps);
+        if(step % output_steps == 0){
+            std::cout << std::flush;
+            printf("\n%d \t %lf \t %lf \t %lf",
+                   step, box[0] * box[1] * box[2],
+                   moveRatio,
+                   volumeRatio);
+            if(step % 10000 == 0 && !measure)
+            {
+                double volume_current = box[0]*box[1]*box[2];
+                if((volume_current < volume_old && !liquid)||(volume_current > volume_old && liquid))
+                {
+                    measure = 1;
+                    std::cout << "\nMetingen zijn begonnen!\n";
+                }
+                volume_old = volume_current;
+            }
+            if(measure)
+            {
+                fprintf(fp,"\n%d \t %lf \t %lf \t %lf",
+                        step, box[0] * box[1] * box[2],
+                        moveRatio,
+                        volumeRatio);
+                measurements++;
+                if (measurements>maxMeasure){fclose(fp);return 1;}
+            }
+            if(moveRatio < 0.35) {delta *= 0.8; std::cout << "\ndelta changed to: " << delta;}
+            if(moveRatio > 0.65) {delta *= 1.2; std::cout << "\ndelta changed to: " << delta;}
+            if(volumeRatio < 0.35) {deltaV *= 0.8; std::cout << "\ndeltaV changed to: " << deltaV;}
+            if(volumeRatio > 0.65) {deltaV *= 1.2; std::cout << "\ndeltaV changed to: " << deltaV;}
+            //std::cout << "Vol accepted: " << vol_accepted<< "\n";
+            move_accepted = 0;
+            vol_accepted = 0;
+            //write_data(step);
+        }
+    }
+    fclose(fp);*/
+
+    return 0;
+}
