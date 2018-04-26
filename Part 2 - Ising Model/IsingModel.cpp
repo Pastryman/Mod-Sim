89fbfe61b2bf392f8ecf288e82dd997ffdfb9d21
TomNiessen <niessen.tom@gmail.com>
2018-04-26 17:54:47 +0200

    Stuff

--- Part 2 - Ising Model/IsingModel.cpp
+++ Part 2 - Ising Model/IsingModel.cpp
@@ -1,17 +1,75 @@
 #include <iostream>
 #include "mt19937.h"
+#include <math.h>
 
-#define N 100
+#define N 3
 
 
-// Parameters
-double J = 1; // J>0: prefers alignment J<0: prefers anti alignment
+// Simulation Parameters
+int output_data_steps = 100;    // Write data to file and console
+int measurements = 1000;        // Amount of measurements we want to do
+int initialize_steps = 100000;  // Amount of steps until we start measuring
+
+// Physical Parameters
+int J = 1; // J>0: prefers alignment J<0: prefers anti alignment
+double Beta = 0.1;
+
+
 
 // Declaration of variables
-double H;
-int M;
+int H; // Hamiltonian
+double m; // Average megnetization
 
 int lattice[N][N];
+
+int H_term(int i, int j, int l,int r,int u,int d){
+    // i belongs to u and d
+    // j belongs to l and r
+
+    int Htemp = 0;
+
+    Htemp += lattice[i][j] * lattice[u][j];
+    Htemp += lattice[i][j] * lattice[d][j];
+    Htemp += lattice[i][j] * lattice[i][l];
+    Htemp += lattice[i][j] * lattice[i][r];
+
+    Htemp += lattice[i][j] * lattice[u][r];
+    Htemp += lattice[i][j] * lattice[u][l];
+    Htemp += lattice[i][j] * lattice[d][r];
+    Htemp += lattice[i][j] * lattice[d][l];
+
+    return Htemp;
+}
+
+int hamiltonian_one(int i, int j){
+    int iplus;
+    int jplus;
+    int imin;
+    int jmin;
+    iplus = (i + 1)%N;
+    jplus = (j + 1)%N;
+    imin = i - 1;
+    jmin = j - 1;
+
+    if (imin<0) {imin = N - 1;}
+    if (jmin<0) {jmin = N - 1;}
+
+    return H_term(i,j,jmin,jplus,imin,iplus);
+}
+
+int hamiltonian() {
+    H = 0;
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            H+=hamiltonian_one(i,j);
+        }
+    }
+    // Apply coupling strength (between spins)
+    H=-H*J/2;
+
+    //std::cout<<"\nH = " << H;
+}
 
 void initialize_lattice(bool random = true) {
 
@@ -31,87 +89,62 @@ void initialize_lattice(bool random = true) {
             }
         }
     }
-
-}
-
-double H_term(int i, int j, int l,int r,int u,int d){
-    // i belongs to u and d
-    // j belongs to l and r
-
-    double Htemp = 0;
-
-    Htemp += lattice[i][j] * lattice[u][j];
-    Htemp += lattice[j][j] * lattice[d][j];
-    Htemp += lattice[i][j] * lattice[i][l];
-    Htemp += lattice[i][j] * lattice[i][r];
-
-    Htemp += lattice[i][j] * lattice[u][r];
-    Htemp += lattice[i][j] * lattice[u][l];
-    Htemp += lattice[i][j] * lattice[d][r];
-    Htemp += lattice[i][j] * lattice[d][l];
-
-    return Htemp;
-}
-
-
-void hamiltonian() {
-
-    int iplus;
-    int jplus;
-    int imin;
-    int jmin;
-    H = 0;
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            iplus = (i + 1)%N;
-            jplus = (j + 1)%N;
-            imin = i - 1;
-            jmin = j - 1;
-
-            if (imin<0) {imin = N - 1;}
-            if (jmin<0) {jmin = N - 1;}
-
-            H+=H_term(i,j,jmin,jplus,imin,iplus);
-        }
-    }
-    // Apply coupling strength (between spins)
-    H=-H*J/2;
-
-    //std::cout<<"\nH = " << H;
+    hamiltonian();
 }
 
 void magnetization(){
-    M = 0;
+    m = 0;
 
     // Loop to get the total magnetization
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            M+=lattice[i][j];
+            m+=lattice[i][j];
         }
     }
 
     // Take the average
-    M/=N*N;
+    m=m/double(N*N);
+}
+
+int attempt_flip(){
+    // Perform flip of random particle
+    int x = int(dsfmt_genrand()*N);
+    int y = int(dsfmt_genrand()*N);
+
+    // The change in energy
+    int dH = -2*hamiltonian_one(x, y);
+
+    // Reject the move if...
+    if(dsfmt_genrand()<exp(-Beta*dH)){
+        lattice[x][y]*=-1;
+       // H+=dH;
+        return 1;
+    }
+    return 0;
+}
+
+void MC_sweep(){
+    for (int n = 0; n < N * N; ++n) {
+        attempt_flip();
+    }
 }
 
 int main() {
     dsfmt_seed(time(NULL));
 
-    initialize_lattice(false);
 
-    lattice[0][0]=-1;
+    initialize_lattice();
+    printf("Initializing done!");
 
-    hamiltonian();
+    for (int step = 0; step < initialize_steps + measurements; ++step) {
+        MC_sweep();
 
-//    int test = 0;
-//    for (int n = 0; n < 10000; ++n) {
-//        initialize_lattice(false);
-//        hamiltonian();
-//        test += H;
-//    }
-
-    std::cout<<"\nfinal result = " << H;
+        if(step%output_data_steps==0){
+            magnetization();
+            hamiltonian();
+            printf("\nstep = %d \t E=%d \t m = %lf",step, H, m);
+        }
+    }
 
     return 0;
 }
